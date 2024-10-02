22631019ff7a8eb9ae5627d26e8cd6872d84671f
hayamaj <hayamaj@bu.edu>
2024-10-02 13:09:43 -0400

    new article

---
+++ components/summer.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { FaLinkedin, FaGithub, FaEnvelope, FaLocationArrow } from "react-icons/fa6";
+import Image from "next/image";
+import { cn } from "@/utils/cn";
+
+const Summer = () => {
+  return (
+    <div className="my-8" id="about">
+      <div className="flex flex-col items-center justify-center p-4 gap-16 mt-4">
+  <div className="flex flex-col md:flex-col items-center justify-center p-4 gap-8 mt-4">
+
+<div className="mt-16">
+                <h1 className="font-medium text-md">A Post-Grad Midlife Crisis?</h1>
+                <div className="flex flex-row gap-2">
+                <h1 className="font-light opacity-50 text-xs text-center">Haya AlMajali &#8212;</h1>
+                <h1 className="font-light opacity-50 text-xs text-center">October 2nd, 2024</h1>
+                </div>
+            </div>
+
+    <p className="lg:text-md lg:font-light font-light font-sans text-sm text-justify max-w-lg text-black opacity-70 pt-4">
+    After graduation, I was hit with a wave of uncertainty. Before completing my degree, I had assumed becoming a Software Engineer was the only logical next step. But once I graduated, things felt less clear. It didn&apos;t seem like the right fit for me. I started questioning where I could make the biggest impact and how I could apply my skills in a way that felt meaningful.    </p>
+    <p className="-mt-4 lg:text-md lg:font-light font-light font-sans text-sm text-justify max-w-lg text-black opacity-70">
+    The first step in addressing this disconnect was reconnecting with the tech industry and the community I wanted to be part of. I realized that to find my path, I needed to grow my network, understand what others were working on, and figure out how I could fit into this rapidly evolving space.
+    </p>
+        <p className="-mt-4 lg:text-md lg:font-light font-light font-sans text-sm text-justify max-w-lg text-black opacity-70">
+        This summer, I clocked in over 50 hours of bus commutes between NYC and Boston. Through cold outreach, I attended my first TechStars event in Boston, followed by various events throughout New York Tech Week, the NEXUS Global Summit, and the AI Furnace conference that just took place last week. Along the way, I connected with entrepreneurs, founders, activists, professors, investors, and directors—some of whom I&apos;ve since had the chance to collaborate with on projects and ideas.</p>
+        <p className="-mt-4 lg:text-md lg:font-light font-light font-sans text-sm text-justify max-w-lg text-black opacity-70">
+        These experiences gave me a deep understanding of the current directions in tech, what people are building, and who the key players are. I also started to see that my role in this space wasn&apos;t just about writing code. It became clear that the most valuable way to apply my CS skills was in understanding how technology impacts people and how it&apos;s applied in the real world. I became fascinated with the intersection of product development and human-centered design.</p>
+        <p className="-mt-4 lg:text-md lg:font-light font-light font-sans text-sm text-justify max-w-lg text-black opacity-70">
+        I&apos;ve learned that it&apos;s tough to figure out your place in an industry you don&apos;t fully understand, especially when roles can be fluid and companies come up with their own titles for positions that might not fit traditional labels like &apos;Software Engineer.&apos; But now, I&apos;m more confident in my direction. I see tech and AI companies as dynamic ecosystems with all kinds of people doing different things to push innovation forward, and I&apos;m excited to find my place in contributing to that ecosystem.
+</p>
+
+  </div>
+  
+
+</div>
+
+
+
+          
+
+      </div>
+      
+  );
+};
+
+export default Summer;
